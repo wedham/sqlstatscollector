f008592b34bbc8e4bf6083d167de841db8fc69da
Mikael Wedham <mikael.wedham@pagesplit.se>
2022-10-11 10:23:09 +0200

    Connection activity check

---
+++ database/sqlstatscollector/data/Tables/connection_properties.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [data].[connection_properties] (
+    [db_name]          NVARCHAR (128) NOT NULL,
+    [host_name]        NVARCHAR (128) NOT NULL,
+    [login_name]       NVARCHAR (128) NOT NULL,
+    [program_name]     NVARCHAR (128) NOT NULL,
+    [connection_count] BIGINT         NOT NULL,
+    [last_seen]        DATETIME2 (0)  NOT NULL
+);
+
